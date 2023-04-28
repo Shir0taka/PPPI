5fdceb321f6f84fa1c06802ad637386f8e256208
Shirotaka <vector.secret@gmail.com>
2023-04-28 14:04:33 +0300

    Done

---
+++ Labs/Lab1/Lab1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exit bool
+            bool exit = false;
+
+            while (!exit)
+            {
+                //Menu
+                Console.WriteLine("Choose option:");
+                Console.WriteLine("1. Set amount of words to show");
+                Console.WriteLine("2. Do math operations");
+                Console.WriteLine("0. Exit");
+
+                //User input
+                int choice = int.Parse(Console.ReadLine());
+
+                //choices
+                switch (choice)
+                {
+                    case 1:
+                        //func 1
+                        Console.WriteLine("Enter a number words:");
+                        ShowWordsInText();
+                        break;
+                    case 2:
+                        //func 2
+                        DoMath();
+                        break;
+                    case 0:
+                        //exit
+                        exit = true;
+                        break;
+                    default:
+                        //error
+                        Console.WriteLine("Incorrect input!");
+                        break;
+                }
+            }
+        }
+
+        static void ShowWordsInText()
+        {
+            string pathFile = "lorem_ipsum.txt";
+            string text = File.ReadAllText(pathFile);
+            int showWords = int.Parse(Console.ReadLine());
+
+            string[] words = text.Split(new char[] {' ', '\t', '\n', '\r'});
+
+            Console.Clear();
+            Console.WriteLine($"Result: {string.Join(" ", words.Take(showWords))} \n");
+        }
+
+        static void DoMath()
+        {
+            //values
+            Console.Write("Enter first value: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter second value: ");
+            double num2 = double.Parse(Console.ReadLine());
+            //operation
+            Console.Write("Select operation (+, -, *, /): ");
+            char operation = Console.ReadLine()[0];
+
+            //Math
+            double res = 0;
+            switch (operation)
+            {
+                case '+':
+                    res = num1 + num2;
+                    Console.Clear();
+                    Console.WriteLine($"Result: {res} \n");
+                    break;
+                case '-':
+                    res = num1 - num2;
+                    Console.Clear();
+                    Console.WriteLine($"Result: {res} \n");
+                    break;
+                case '*':
+                    res = num1 * num2;
+                    Console.Clear();
+                    Console.WriteLine($"Result: {res} \n");
+                    break;
+                case '/':
+                    res = num1 / num2;
+                    Console.Clear();
+                    Console.WriteLine($"Result: {res} \n");
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Incorrect operation! \n");
+                    return;
+            }
+        }
+    }
+}
